b0e90299a3eb0a7655860a27e77d5afeda0aaa0a
Tom Rabone <tommy@conquerai.co.uk>
2026-03-11 16:52:27 +0000

    Added sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://auto-cleanse.co.uk'
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
